e961945e55ec74e2f31be212800cc372cbf035f9
R.Hasanzada <ruslan.hasanzada@gmail.com>
2020-06-28 17:28:54 +0400

    add packages

--- src/main/java/com/example/url_shortner/UrlShortnerApplication.java
+++ src/main/java/com/example/url_shortner/UrlShortnerApplication.java
@@ -1,4 +1,4 @@
-package com.url_shortner;
+package com.example.url_shortner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
